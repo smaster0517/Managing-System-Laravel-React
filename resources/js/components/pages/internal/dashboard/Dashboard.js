dc2731c0da324dc0d52271596e67d2d1af1cc9dd
smaster0517 <smaster0517@gmail.com>
2022-08-18 03:25:27 -0300

    dashboard metrics

--- resources/js/components/pages/internal/dashboard/Dashboard.js
+++ resources/js/components/pages/internal/dashboard/Dashboard.js
@@ -1,43 +1,106 @@
 import React from 'react';
 // Material UI
 import Toolbar from '@mui/material/Toolbar';
-import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CircularProgress from '@mui/material/CircularProgress';
+// Fonts Awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faMap } from '@fortawesome/free-solid-svg-icons';
+import { faClipboard } from '@fortawesome/free-solid-svg-icons';
+import { faChartColumn } from '@fortawesome/free-solid-svg-icons';
 // Custom
-import { useEffect } from "react";
+//import AxiosApi from '../../../../services/AxiosApi';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+export const Dashboard = React.memo(({ ...props }) => {
 
-export const Dashboard = React.memo(({...props}) => {
-
-  useEffect(() => {
+  React.useEffect(() => {
     props.setPage("DASHBOARD");
+
+
+
   }, []);
 
   return (
-    <Paper sx={{ maxWidth: "95%", margin: 'auto', overflow: 'hidden', borderRadius: 5, padding: 3 }}>
+    <>
+      <Paper sx={{ maxWidth: "90%", margin: 'auto', overflow: 'hidden', borderRadius: 5 }}>
 
-      <Toolbar>
-        <Grid container spacing={2} columns={12}>
-          <Grid item xs={12} md={6} lg={6}>
-            <Item>ITEM A</Item>
+        <Toolbar>
+          <Grid container spacing={2}>
+
+            <Grid item xs={12} md={6} lg={6} xl={3}>
+              <Card sx={{ minWidth: 200, maxWidth: 450, margin: 'auto' }}>
+                <CardContent>
+                  <Typography variant="h5" component="div" textAlign="center" mb={1}>
+                    <FontAwesomeIcon icon={faUsers} color="green" />
+                  </Typography>
+                  <Typography variant="h5" component="div" textAlign="center" mb={1}>
+                    Usuários
+                  </Typography>
+                  <Typography variant="h5" component="div" textAlign="center">
+                    <CircularProgress />
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={12} md={6} lg={6} xl={3}>
+              <Card sx={{ minWidth: 200, maxWidth: 450, margin: 'auto' }}>
+                <CardContent>
+                  <Typography variant="h5" component="div" textAlign="center" mb={1}>
+                    <FontAwesomeIcon icon={faMap} color="green" />
+                  </Typography>
+                  <Typography variant="h5" component="div" textAlign="center" mb={1}>
+                    Planos de voo
+                  </Typography>
+                  <Typography variant="h5" component="div" textAlign="center">
+                    <CircularProgress />
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={12} md={6} lg={6} xl={3}>
+              <Card sx={{ minWidth: 200, maxWidth: 450, margin: 'auto' }}>
+                <CardContent>
+                  <Typography variant="h5" component="div" textAlign="center" mb={1}>
+                    <FontAwesomeIcon icon={faClipboard} color="green" />
+                  </Typography>
+                  <Typography variant="h5" component="div" textAlign="center" mb={1}>
+                    Ordens de serviço
+                  </Typography>
+                  <Typography variant="h5" component="div" textAlign="center">
+                    <CircularProgress />
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={12} md={6} lg={6} xl={3}>
+              <Card sx={{ minWidth: 200, maxWidth: 450, margin: 'auto' }}>
+                <CardContent>
+                  <Typography variant="h5" component="div" textAlign="center" mb={1}>
+                    <FontAwesomeIcon icon={faChartColumn} color="green" />
+                  </Typography>
+                  <Typography variant="h5" component="div" textAlign="center" mb={1}>
+                    Relatórios
+                  </Typography>
+                  <Typography variant="h5" component="div" textAlign="center">
+                    <CircularProgress />
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
           </Grid>
-          <Grid item xs={12} md={6} lg={6}>
-            <Item>ITEM B</Item>
-          </Grid>
-          <Grid item xs={12} md={12} lg={12}>
-            <Item>ITEM C</Item>
-          </Grid>
-        </Grid>
-      </Toolbar>
-    </Paper>
+        </Toolbar>
+      </Paper>
+    </>
+
   )
-  
+
 });
